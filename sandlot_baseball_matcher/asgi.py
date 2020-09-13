bac61a224be794a107b78130e70ceb26efb64df6
Kento Iwasaki <rockiwasaki1984@gmail.com>
2020-09-13 21:07:26 +0900

    initial commit

---
+++ sandlot_baseball_matcher/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sandlot_baseball_matcher project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sandlot_baseball_matcher.settings')
+
+application = get_asgi_application()
